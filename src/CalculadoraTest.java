f30a69d73739e36b0eccb8b7ea06f2955682cab9
konstanZX2 <zxgenerationzx@gmail.com>
2022-02-24 16:00:06 +0100

    si

--- src/CalculadoraTest.java
+++ src/CalculadoraTest.java
@@ -1,3 +1,5 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculadoraTest {
@@ -7,5 +9,18 @@ class CalculadoraTest {
         int resultado = Calculadora.suma(2,3);
         int esperado = 5;
         assertEquals(esperado,resultado);
+    }
+
+    @Test
+    void resta() {
+        int resultado = Calculadora.resta(8,3);
+        int esperado = 5;
+        assertEquals(esperado,resultado);
+    }
+
+    @Test
+    void mayor50() {
+        boolean resultado = Calculadora.mayor50(5000000);
+        assertTrue(resultado);
     }
 }
